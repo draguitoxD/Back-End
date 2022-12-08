60e54da1dc9a6e7d2f821f8662a5fc9bf1709663
draguitoxD <nazarenoviero@gmail.com>
2022-12-08 17:13:25 -0300

    spring add

---
+++ miprimerportfolio/src/main/java/com/miportfolio/miprimerportfolio/MiprimerportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.miportfolio.miprimerportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprimerportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiprimerportfolioApplication.class, args);
+	}
+
+}
---
+++ miprimerportfolio/src/test/java/com/miportfolio/miprimerportfolio/MiprimerportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miportfolio.miprimerportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprimerportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
