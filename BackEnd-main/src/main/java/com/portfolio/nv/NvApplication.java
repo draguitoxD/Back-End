3f068d4a46e3853cde872c3f76cbab433943ff13
draguitoxD <nazarenoviero@gmail.com>
2023-01-08 21:45:16 -0300

    Agrego repo del backend

---
+++ BackEnd-main/src/main/java/com/portfolio/nv/NvApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.nv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvApplication.class, args);
+	}
+
+}
---
+++ BackEnd-main/src/main/java/com/portfolio/nv/Security/Service/RolService.java
@@ -0,0 +1,24 @@
+package com.portfolio.nv.Security.Service;
+
+import com.portfolio.nv.Security.Entity.Rol;
+import com.portfolio.nv.Security.Enums.RolNombre;
+import com.portfolio.nv.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
