8ca4c7bcbb260be675d8f6736d96c988fc955d3a
draguitoxD <nazarenoviero@gmail.com>
2023-01-11 19:12:32 -0300

    URL Cambios

--- src/main/java/com/portfolio/nv/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/nv/Controller/CExperiencia.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("explab")
+@CrossOrigin(origins = "https://front-end-porfolio-7915c.web.app")
+@RequestMapping("/explab")
 public class CExperiencia {
 
     @Autowired
