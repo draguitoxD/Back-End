088c1b463523794d97d58249d75656571d947754
draguitoxD <nazarenoviero@gmail.com>
2023-03-06 21:13:36 -0300

    Update para subir a render

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:19-alpine-jdk
+MAINTAINER nazarenoviero
+COPY target/nv-0.0.1-SNAPSHOT.jar nv-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/nv-0.0.1-SNAPSHOT.jar"]
--- src/test/java/com/portfolio/nv/NvApplicationTests.java
+++ src/test/java/com/portfolio/nv/NvApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfolio.mgb;
+package com.portfolio.nv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MgbApplicationTests {
+class NvApplicationTests {
 
 	@Test
 	void contextLoads() {
